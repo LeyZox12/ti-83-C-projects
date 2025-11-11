718315497cd1c826017288285a50583001860b67
Leo <161898497+LeyZox12@users.noreply.github.com>
2025-11-11 23:01:48 +0100

    ported to TI, still some bugs to fix
    bugs to fix:
    random crash on calc(not on emulator->probably memory leak)
    no check for rotation collision
    when piece is far left, it disapear

--- ctris.c
+++ ctris.c
@@ -1,18 +1,20 @@
-#include "raylib.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-#define bint uint8_t
+#include "graphx.h"
+#include "keypadc.h"
+#include "sys/timers.h"
+#include "tice.h"
+#include "sys/rtc.h"
 
 struct Piece {
-  bint *mat;
-  bint size;
+  uint8_t *mat;
+  uint8_t size;
 };
 
-void initPiece(struct Piece *p, bint size);
-void set(struct Piece *p, bint x, bint y, bint val);
+void initPiece(struct Piece *p, uint8_t size);
+void set(struct Piece *p, uint8_t x, uint8_t y, uint8_t val);
 void initPieces();
 void printPiece(struct Piece *p);
 void getPiece(struct Piece **ptr);
@@ -21,9 +23,15 @@ void down(struct Piece *p);
 void left(struct Piece *p);
 void right(struct Piece *p);
 void rotate(struct Piece *p);
-void drawGrid(bint* grid);
-bool checkCollision(struct Piece* p, bint* mat, bint xOffset, bint yOffset, bint rotOffset);
+void drawGrid(uint8_t * grid);
 void clearMat();
+void leftRot(struct Piece *p);
+void rightRot(struct Piece *p);
+bool checkCollision(struct Piece* p, uint8_t * mat, uint8_t xOffset, uint8_t yOffset, uint8_t rotOffset);
+
+clock_t timer();
+
+bool inMenu = false;
 
 struct Piece T;
 struct Piece S;
@@ -32,58 +40,140 @@ struct Piece I;
 struct Piece L;
 struct Piece J;
 struct Piece O;
-bint x, y;
-const bint gridX = 10;
-const bint gridY = 14;
-const int pixelSize = 50;
-bint* mat;
+uint8_t x, y;
+const uint8_t gridX = 10;
+const uint8_t gridY = 14;
+const int pixelSize = 240/gridY;
+const int defaultFrameTime = 300;
+clock_t frameTime = 300;
+uint8_t * mat;
 bool isPiece = false;
 
+bool paused = false;
+
+uint8_t * linesToClear;
+uint8_t lineToClearSize;
+
+clock_t moveStart;
+clock_t frameStart;
+int animatingFrames = -1;
+
+
 int main(void) {
+
+  timer_Disable(1);
+  timer_Set(1, 0);
+  timer_SetReload(1, 0);
+  timer_Enable(1, TIMER_32K, TIMER_NOINT, TIMER_UP);
+
+  frameStart = timer();
+  linesToClear = (uint8_t *)malloc(sizeof(uint8_t ) * gridY);
+  moveStart = timer();
   x = 5;
   y = 0;
   srand(time(NULL));
-  const int screenWidth = 800;
-  const int screenHeight = 800;
+  const int screenWidth = 320;
+  const int screenHeight = 240;
   const int wallSize = 100;
-  clock_t start = clock();
-
-  mat = (bint*)malloc(sizeof(bint) * gridX * gridY);
+  clock_t start = timer();
+  mat = (uint8_t *)malloc(sizeof(uint8_t ) * gridX * gridY);
   clearMat();
   struct Piece *currPiece;
   initPieces();
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-  SetTargetFPS(60);
-  while (!WindowShouldClose()) {
+  kb_key_t key;
+  gfx_Begin();
 
+  bool upPressed = false;
 
-    if (!isPiece) {
-      getPiece(&currPiece);
-      isPiece = true;
+  while (kb_Data[1] != kb_2nd) {
+  
+  {
+    //gfx_PrintString(timer());
+    gfx_SetDrawBuffer();
+    frameStart = timer();
+    kb_Scan();
+    key = kb_Data[7];
+    if(kb_Data[6] == kb_Enter) paused = !paused;
+      if(paused)  
+      {
+
+      }
+      else if(animatingFrames == -1)
+      {
+        if (!isPiece) {
+          getPiece(&currPiece);
+          isPiece = true;
+        }
+        if(key == kb_Left && timer() - moveStart > 150) left(currPiece);
+        else if(key == kb_Right && timer() - moveStart > 150) right(currPiece);
+        if(timer() - start > frameTime)
+        {
+          start = timer();
+          down(currPiece);
+        }
+        if(key == kb_Up && !upPressed)
+        {
+          upPressed = true; 
+          rightRot(currPiece);
+        }
+        else if(key != kb_Up) upPressed = false;
+        if(key == kb_Down) frameTime = 50;
+        else if(frameTime != defaultFrameTime)
+        {
+          frameTime = defaultFrameTime;
+          start = timer();
+        }
+        for(int i = 0; i < gridY; i++)
+        {
+          uint8_t count = 0;
+          for(int j = 0; j < gridX; j++)
+          {
+            if(mat[i * gridX+j] == 1) count++; 
+          }
+          if(count==10)
+          {
+            linesToClear[lineToClearSize] = i;
+            lineToClearSize++;
+            animatingFrames = 5;
+          }
+        }
+      }
+      else 
+      {
+        for(int i = 0; i < lineToClearSize; i++)
+        {
+          mat[linesToClear[i] * gridX + 9  - animatingFrames] = 0;
+          mat[linesToClear[i] * gridX + animatingFrames] = 0;
+        }
+        start = timer();
+        if(--animatingFrames == -1)
+        {
+          for(int i = 0; i < lineToClearSize; i++)
+          {
+            int y = linesToClear[i];
+            for(int j = y-1; j >= 0; j--)
+            {
+              for(int px = 0; px < gridX; px++)
+              {
+                mat[(j+1)*gridX + px] = mat[(j)*gridX + px];
+              }
+            }
+          }
+          lineToClearSize = 0;
+        }
+      }
+      gfx_FillScreen(0);
+      gfx_SetColor(255);
+      gfx_FillRectangle(0, 0, gridX * pixelSize, gridY * pixelSize);
+      drawPiece(currPiece);
+      drawGrid(mat);
+      gfx_BlitBuffer();
+      
     }
-
-    if(clock() - start > 300)
-    {
-      start = clock();
-      down(currPiece);
-    }
-    if(IsKeyPressed(KEY_LEFT)) left(currPiece );
-    if(IsKeyPressed(KEY_RIGHT)) right(currPiece );
-    if(IsKeyPressed(KEY_UP)) rotate(currPiece);
-
-    BeginDrawing();
-    ClearBackground(BLACK);
-    DrawRectangle(0, 0, gridX * pixelSize, gridY * pixelSize, WHITE);
-    drawPiece(currPiece);
-    drawGrid(mat);
-    for (int i = 0; i < gridX; i++)
-      DrawLine(pixelSize * i, 0, pixelSize * i, pixelSize * gridY, GRAY);
-    for (int i = 0; i < gridY; i++)
-      DrawLine(0, pixelSize * i, pixelSize * gridX, pixelSize * i, GRAY);
-    EndDrawing();
   }
-  CloseWindow();
+  gfx_End();
   free(currPiece->mat);
+  free(linesToClear);
   free(mat);
   free(T.mat);
   free(S.mat);
@@ -95,9 +185,9 @@ int main(void) {
   return 0;
 }
 
-void initPiece(struct Piece *p, bint size) {
+void initPiece(struct Piece *p, uint8_t size) {
   p->size = size;
-  p->mat = (bint *)malloc(sizeof(bint) * size * size);
+  p->mat = (uint8_t *)malloc(sizeof(uint8_t ) * size * size);
   for (int i = 0; i < size * size; i++)
     p->mat[i] = 0;
 }
@@ -111,7 +201,7 @@ void printPiece(struct Piece *p) {
   printf("\n");
 }
 
-void set(struct Piece *p, bint x, bint y, bint val) {
+void set(struct Piece *p, uint8_t x, uint8_t y, uint8_t val) {
   p->mat[x + y * p->size] = val;
 }
 
@@ -138,15 +228,15 @@ void initPieces() {
   Z.mat[7] = 1;
   Z.mat[8] = 1;
 
-  L.mat[0] = 1;
-  L.mat[3] = 1;
-  L.mat[6] = 1;
+  L.mat[1] = 1;
+  L.mat[4] = 1;
   L.mat[7] = 1;
+  L.mat[8] = 1;
 
-  J.mat[2] = 1;
-  J.mat[5] = 1;
-  J.mat[8] = 1;
+  J.mat[1] = 1;
+  J.mat[4] = 1;
   J.mat[7] = 1;
+  J.mat[6] = 1;
 
   O.mat[0] = 1;
   O.mat[1] = 1;
@@ -191,11 +281,12 @@ void getPiece(struct Piece **ptr) {
 
 void drawPiece(struct Piece *p)
 {
+  gfx_SetColor(24);
   for(int i = 0; i < p->size; i++){
     for(int j = 0; j < p->size; j++)
     {
       if(p->mat[i*p->size + j] == 1) 
-        DrawRectangle((x + j) * pixelSize, (y + i) * pixelSize, pixelSize, pixelSize,  BLUE); 
+        gfx_FillRectangle((x + j) * pixelSize, (y + i) * pixelSize, pixelSize, pixelSize); 
     }
   }
 }
@@ -207,7 +298,7 @@ void down(struct Piece *p)
   else
   {
     isPiece = false;
-    bint px, py;
+    uint8_t px, py;
     for(int i = 0; i < p->size; i++){
       for(int j = 0; j < p->size; j++)
       {
@@ -222,13 +313,19 @@ void down(struct Piece *p)
 void left(struct Piece *p)
 {
   if(!checkCollision(p, mat, -1, 0, 0))
+  {
     x--;
+    moveStart = timer();
+  }
 }
 
 void right(struct Piece *p)
 {
   if(!checkCollision(p, mat, 1, 0, 0))
+  {
     x++;
+    moveStart = timer();
+  }
 }
 
 void rotate(struct Piece *p)
@@ -236,13 +333,13 @@ void rotate(struct Piece *p)
 
 }
 
-bool checkCollision(struct Piece* p, bint* mat, bint xOffset, bint yOffset, bint rotOffset)
+bool checkCollision(struct Piece* p, uint8_t * mat, uint8_t xOffset, uint8_t yOffset, uint8_t rotOffset)
 {
   if(rotOffset != 0)
   {
 
   }
-  bint px, py;
+  uint8_t px, py;
   for(int i = 0; i < p->size; i++){
     for(int j = 0; j < p->size; j++)
     {
@@ -255,14 +352,15 @@ bool checkCollision(struct Piece* p, bint* mat, bint xOffset, bint yOffset, bint
   return false;
 }
 
-void drawGrid(bint* grid)
+void drawGrid(uint8_t * grid)
 {
+  gfx_SetColor(24);
   for(int i = 0; i < gridY; i++)
   {
     for(int j = 0; j < gridX; j++)
     {
       if(grid[j + i * gridX] == 1) 
-        DrawRectangle(j * pixelSize, i * pixelSize, pixelSize, pixelSize,  BLUE); 
+        gfx_FillRectangle(j * pixelSize, i * pixelSize, pixelSize, pixelSize); 
     }
   }
 }
@@ -272,6 +370,26 @@ void clearMat()
   for(int i = 0; i < gridX * gridY; i++) mat[i]=0;
 }
 
-// U:\MinGW\bin\gcc.exe main.c -o ctris -Iinclude
-// -IU:\MinGW\x86_64-w64-mingw32\include -Llib -lraylibdll
-// gcc main.c -o ctris -Iinclude -Llib -lraylibdll
+void leftRot(struct Piece *p)
+{
+}
+
+void rightRot(struct Piece *p)
+{
+  uint8_t * buffer;
+  buffer = (uint8_t *)malloc(sizeof(uint8_t )* p->size * p->size);
+  for(int i = 0; i < p->size * p->size; i++) buffer[i] = p->mat[i];
+  uint8_t x, y;
+  for(int i = 0; i < p->size * p->size; i++)
+  {
+    x = i%p->size;
+    y = i/p->size;
+    p->mat[i] = buffer[(p->size - x-1) * p->size + y];
+  }
+  free(buffer);
+}
+
+clock_t timer()
+{
+  return (timer_Get(1) * 1000) / 32768;
+}
